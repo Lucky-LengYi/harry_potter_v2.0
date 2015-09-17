35abe68b71dac930fa1a7315f8dd580dea4879ed
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-09-17 21:39:02 +0800

    add the book-spec

---
+++ spec/book-spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Book = require('../src/model/book');
+
+describe('Book', function() {
+  it('should have two attribute', function() {
+    var book = new Book('partOne', 8);
+    expect(book.name).toBe('partOne');
+    expect(book.price).toBe(8);
+  });
+});
